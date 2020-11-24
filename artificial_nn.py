45d4edfcf9325195ba4899271d47bb21f7c3250c
WilliamRWalsh <williamrobertsonwalsh@gmail.com>
2020-11-24 00:27:39 -0500

    some clean up and docstrings

--- artificial_nn.py
+++ artificial_nn.py
@@ -3,24 +3,23 @@ from keras.datasets import mnist
 
 
 class ArtificialNeuralNetwork():
+    '''
+    Simple Artificial Neural Network to classify handwritten digits.
+    '''
+
     def __init__(self, alpha):
-
-        (self.training_X, self.training_y), (self.test_X,
-                                             self.test_y) = mnist.load_data()
-        self.training_X = self.training_X[:100]
-        self.training_y = self.training_y[:100]
-        self.training_y = self.training_y.reshape(100, 1)
-        x = [x.flatten() for x in self.training_X]
-
-        self.training_X = np.asarray(x)
-
         self.alpha = alpha
 
-        # Training Data
-        # self.training_X = np.array(
-        #     ([2, 2], [4, 4], [6, 6], [8, 8]), dtype=float)
-        # self.training_y = np.array(([20], [40], [60], [80]), dtype=float)
+        # Training & Testing Data
+        num_samples = 10
+        (self.training_X, self.training_y), (self.test_X,
+                                             self.test_y) = mnist.load_data()
+        self.training_X = self.training_X[:num_samples]
+        self.training_y = self.training_y[:num_samples].reshape(num_samples, 1)
+        # TODO: Try using rivel instead of flatten
+        self.training_X = np.asarray([x.flatten() for x in self.training_X])
 
+        # Normalize inputs/outputs
         self.training_X = self.training_X / 255
         self.training_y = self.training_y / 9
 
@@ -29,6 +28,7 @@ class ArtificialNeuralNetwork():
         self.hidden_layer_1_size = 64
         self.output_layer_size = 1
 
+        # Weights & Biases
         self.W_1 = np.random.randn(
             self.input_layer_size, self.hidden_layer_1_size)
         self.W_2 = np.random\
@@ -37,15 +37,31 @@ class ArtificialNeuralNetwork():
         self.b_2 = np.zeros(self.output_layer_size)
 
     def train(self, iterations):
+        '''
+        Loops over the training data `iterations`# of times updating
+        the weights and biases each time.
+
+        Each loop goes:
+            - Forward pass
+            - Back Propagation
+            - Change Weights
+        '''
         for _ in range(iterations):
             neurons_io = self.forward_pass()
             dJW1, dJW2 = self.back_propgation(neurons_io)
-            self.change_wieghts_and_bias(dJW1, dJW2)
+            self.change_weights_and_bias(dJW1, dJW2)
+
+        # Logs
         print("y = " + str(self.training_y[:10]))
         print("y_hat = " + str(neurons_io['y_hat'][:10]))
         print("Cost = " + str(sum(self.cost_function())))
 
     def forward_pass(self):
+        '''
+        ŷ = f(X * W)
+
+        :return dict: Each neurons inputs/outputs
+        '''
         z_2 = np.dot(self.training_X, self.W_1) + self.b_1
         a_2 = self.sigmoid(z_2)
 
@@ -55,6 +71,10 @@ class ArtificialNeuralNetwork():
         return {'z_2': z_2, 'z_3': z_3, 'a_2': a_2, 'y_hat': a_3}
 
     def back_propgation(self, neurons_io):
+        '''
+        dJ/dW2 = aT d3
+        dJ/dW1 = XT d2
+        '''
         delta3 = np.multiply(
             -(self.training_y - neurons_io['y_hat']
               ), self.sigmoid_prime(neurons_io['z_3'])
@@ -67,43 +87,51 @@ class ArtificialNeuralNetwork():
 
         return dJdW1, dJdW2
 
-    def change_wieghts_and_bias(self, dJW1, dJW2):
+    def change_weights_and_bias(self, dJW1, dJW2):
+        '''
+        Update weights and biases
+        '''
+        # TODO: Add baises
         self.W_1 += -self.alpha * dJW1
         self.W_2 += -self.alpha * dJW2
 
     def cost_function(self):
-        """
+        '''
         J = Σ 1/2(y - ŷ)^2
-        """
+        '''
         io = self.forward_pass()
         x = 1/2 * (self.training_y - io['y_hat']) ** 2
         return x
 
     @staticmethod
     def sigmoid(z):
-        """
-        Activation function
-        """
+        '''
+        Activation function -> f(z)
+        '''
         return 1/(1+np.exp(-z))
 
     @staticmethod
     def sigmoid_prime(z):
+        '''
+        Activation function dervitive -> f'(z)
+        '''
         return np.exp(-z)/((1+np.exp(-z))**2)
 
     def test(self, x):
         self.training_X = x / np.amax(x, axis=0)
 
 
+'''
+Main
+'''
 ann = ArtificialNeuralNetwork(alpha=1)
 ann.train(10000)
 
-# x = np.array(
-#     ([1, 1], [2, 8], [6, 12], [10, 20]), dtype=float)
-# ann.test(x)
-
 """
 Next steps:
-    - How to normalize input data?
-    - Hook it up to the MNIST data
     - Have the training stop after cost is below threshold?
+    - Make 10 output nodes
+    - Make extra hidden layers
+    - Add bais
+    
 """
